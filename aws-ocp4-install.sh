57f4a261f3a2682912b72e13c57647628d7d8980
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2024-12-02 23:13:16 +0100

    🎨 Small tweaks and prechecks

--- aws-ocp4-install.sh
+++ aws-ocp4-install.sh
@@ -24,8 +24,7 @@ ARGOCD_NAMESPACE="gitops"
 ARGOCD_CLUSTER_NAME="argocd"
 
 K_DEFAULT_USER="redhat"
-K_DEFAULT_PASSWD="redhat!1"
-
+K_DEFAULT_PASSWD="${K_DEFAULT_PASSWD:-redhat!1}"
 
 # functs
 OC_PATH="$CLUSTER_WORKDIR/oc"
@@ -40,6 +39,15 @@ function checkVariable {
     fi
 }
 
+# Random suffix generator
+function generate_random_password {
+    tr -dc 'a-z0-9' </dev/urandom | head -c 15
+}
+
+echo -e "\n================="
+echo -e "=   PRE-CHECKS  ="
+echo -e "=================\n"
+
 
 ### PREREQUISITES ### 
 checkVariable "AWS_ACCESS_KEY_ID"
@@ -47,18 +55,39 @@ checkVariable "AWS_SECRET_ACCESS_KEY"
 checkVariable "AWS_DEFAULT_REGION"
 checkVariable "INSTALL_LETS_ENCRYPT_CERTIFICATES"
 
+if [ -f ${HTPASSWD_PATH:-auth/users.htpasswd} ]; then
+
+    echo -e "\nThe users htpasswd file exists."
+
+else 
+    echo -e "\nThe users file does not exist, we will generate a user/password file."
+    K_DEFAULT_PASSWD=$(generate_random_password)
+    users_file=$(mktemp -t users.htpasswd_${f})
+    htpasswd -b -B $users_file redhat $K_DEFAULT_PASSWD
+fi 
+
+
 if command -v htpasswd &> /dev/null; then
-    if ! htpasswd -vb auth/users.htpasswd $K_DEFAULT_USER $K_DEFAULT_PASSWD; then
+    if ! htpasswd -vb "${HTPASSWD_PATH:-auth/users.htpasswd}" $K_DEFAULT_USER $K_DEFAULT_PASSWD; then
         echo "Password verification failed for user $K_DEFAULT_USER or user does not exist"
+        echo "Check line 27 and "
         exit 1
-    else
-        echo "User / password verification went well."
     fi
 else
     echo "htpasswd command not found"
     echo "You should install it if you want this verification."
     echo "In Fedora: sudo dnf install httpd-tools"
-    echo ""
+fi
+
+if command -v podman &>/dev/null; then
+    echo "Podman is installed."
+else
+    echo "Podman is not installed."
+fi
+
+if ! command -v podman &>/dev/null && [[ ${INSTALL_LETS_ENCRYPT_CERTIFICATES} =~ ^([Tt]rue|[Yy]es|[1])$ ]]; then
+    echo "Podman is not installed, and INSTALL_LETS_ENCRYPT_CERTIFICATES is set to True/Yes/1."
+    echo "Exiting. Please, install podman."
 fi
 
 #### Print Variables ####
@@ -76,6 +105,12 @@ echo AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 echo RHOCM_PULL_SECRET=$RHOCM_PULL_SECRET
 echo INSTALL_LETS_ENCRYPT_CERTIFICATES=$INSTALL_LETS_ENCRYPT_CERTIFICATES
 echo ------------------------------------
+
+
+echo -e "\n============================="
+echo -e "=   OPENSHIFT INSTALLATION  ="
+echo -e "=============================\n"
+
 
 # #### AWS ####
 
@@ -121,16 +156,20 @@ OCP_API=https://api.$CLUSTER_NAME.$RHPDS_TOP_LEVEL_ROUTE53_DOMAIN:6443
 
 echo -e "\t- kubeadmin password: $KUBEADMIN_PASSWORD"
 echo -e "\t- Cluster api url: $OCP_API"
+echo ""
 
 oc login -u kubeadmin -p $KUBEADMIN_PASSWORD $OCP_API --insecure-skip-tls-verify=true
 
-oc apply -k auth
-
-echo "Waiting some time to get OAuth configured..."
-sleep 30
+oc create secret generic htpass-secret -n openshift-config --from-file=htpasswd="${HTPASSWD_PATH:-auth/users.htpasswd}"
+oc apply -f auth/oauth-cluster.yaml
+oc apply -f "${GROUP_FILE_PATH:-auth/group-cluster-admins.yaml}"
+oc apply -f auth/clusterrolebinding-cluster-admins.yaml
 
 # Do not add default user as a group, but directly admin (It does not inherit access to gitOps)
 oc adm policy add-cluster-role-to-user cluster-admin ${K_DEFAULT_USER}
+
+echo "Waiting some time to get OAuth configured..."
+sleep 30
 
 # auth
 echo -n "\nWaiting for authentication configuration to be ready..."
